7d297db669fe68a774d03ae7e5930b64c9c9936d
Nate Hawthorne <natejhawthorne@gmail.com>
2016-04-06 11:15:24 -0500

    Got some information on the page

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -7,9 +7,12 @@ import tiy from './app.js';
 // Looks like the imported module was a function, because here we're executing it!
 tiy();
 
-var ShirtInfo = Backbone.View.extend({
-	tagName: 'div',
-	initialize: function() {
+import Backbone from 'backbone';
+import $ from 'jquery';
+
+const ShirtInfo = Backbone.View.extend({
+	tagName: 'section',
+	initialize: function(frontPic, backPic, picTitle, price) {
 		this.frontPic = frontPic;
 		this.backPic = backPic;
 		this.picTitle = picTitle;
@@ -17,13 +20,17 @@ var ShirtInfo = Backbone.View.extend({
 		this.render();
 	},
 	render: function() {
-		var $frontPhoto = $('<img />', {src: this.frontPic, class: 'frontPic'});
-		var $backPhoto = $('<img />', {src: this.backPic});
-		var $picTitle = $('<h4 />', {class: 'picTitle'});
-		var $price = $('<h4 />', {class: 'price'});
+		const $frontPhoto = $('<img />', {src: this.frontPic, class: 'frontPic'});
+		const $backPhoto = $('<img />', {src: this.backPic, class: 'backPic'});
+		const $picTitle = $('<div />', {text: this.picTitle, class: 'picTitle'});
+		const $price = $('<div />', {text: this.price, class: 'price'});
+		this.$el.append($frontPhoto).append($backPhoto).append($picTitle).append($price);
 	}
 });
 
-var shirt1 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Save my Trees", 19);
-var shirt2 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Nature Lover", 19);
-var shirt3 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Forrest Walk", 19);
+const shirt1 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Save my Trees", 19);
+const shirt2 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Nature Lover", 19);
+const shirt3 = new ShirtInfo("http://fillmurray.com/250/250", "http://placecage.com/250/250", "Forrest Walk", 19);
+$('main').append(shirt1.el);
+$('main').append(shirt2.el);
+$('main').append(shirt3.el);
